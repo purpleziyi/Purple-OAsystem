b2cc44fb043152aea6e00abc26e93d5a3982e601
ziyiz <ziyiz@ulrik.uio.no>
2024-06-12 12:38:24 +0200

    activate staff account

--- apps/staff/urls.py
+++ apps/staff/urls.py
@@ -6,5 +6,5 @@ app_name = 'staff'
 urlpatterns = [
     path('departments', views.DepartmentListView.as_view(), name='departments'),
     path('staff', views.StaffView.as_view(), name='staff_view'),
-    # path('active', views.ActiveStaffView.as_view(), name='active_staff')
+    path('active', views.ActiveStaffView.as_view(), name='active_staff')
 ]
--- apps/staff/views.py
+++ apps/staff/views.py
@@ -8,13 +8,25 @@ from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.conf import settings
+from utils import aeser
+from django.urls import reverse  #将view的名字反转成链接
+
+
 
 OAUser = get_user_model()
+
+aes = aeser.AESCipher(settings.SECRET_KEY)
 
 # Create your views here.
 class DepartmentListView(ListAPIView):
     queryset = OADepartment.objects.all()
     serializer_class = DepartmentSerializer
+
+class ActiveStaffView(APIView):
+    def get(self, request):
+        return Response()
+        # pass
+
 
 class StaffView(APIView):
     # 新增员工
@@ -34,9 +46,22 @@ class StaffView(APIView):
             user.save()
 
             # 2. 发送激活邮件
-            send_mail(f'Ziyi active account', recipient_list=[email],message='Ziyi active account', from_email=settings.DEFAULT_FROM_EMAIL)
-            # self.send_active_email(email)
+            # send_mail(f'Ziyi active account', recipient_list=[email],message='Ziyi active account', from_email=settings.DEFAULT_FROM_EMAIL)
+            self.send_active_email(email)
 
             return Response()
         else:
             return Response(data={'detail': list(serializer.errors.values())[0][0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    def send_active_email(self, email):
+        token = aes.encrypt(email)
+        # /staff/active?token=xxx
+        active_path = reverse("staff:active_staff") + "?token=" + token
+        # http://127.0.0.1:8000/staff/active?token=xxx
+        active_url = self.request.build_absolute_uri(active_path)  # 构建绝对的uri
+        # 发送一个链接，让用户点击这个链接后，跳转到激活的页面，才能激活。
+        # 为了区分用户，在发送链接邮件中，该链接中应该要包含这个用户的邮箱
+        # 针对邮箱要进行加密：AES
+        # http://localhost:8000/staff/active?token=4dFLaXTbbzciZKGm0LIafmhOuuW11S+7kEtqdUSeFf4=
+        message = f"Please click the following link to activate your account: {active_url}"  #账号激活
+        send_mail(f'[purpleOA] Account activation', recipient_list=[email], message=message, from_email=settings.DEFAULT_FROM_EMAIL)
